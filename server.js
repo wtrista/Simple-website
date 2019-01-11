a3d282a641878a4b8a11237086fb5fbe5b30ba6f
Trista Wang <trista.x.wang@ibm.com>
2019-01-11 17:04:40 -0500

    routing pages

--- server.js
+++ server.js
@@ -15,3 +15,22 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.get('/About_you', (req, res) => {
+    res.render('you', {
+        title: 'Hey',
+        message: 'How you doin <3'
+    })
+})
+app.get('/Still_you', (req, res) => {
+    res.render('you', {
+        title: 'Yo',
+        message: 'What you upto?'
+    })
+})
+app.get('/And_you_again', (req, res) => {
+    res.render('you', {
+        title: 'Sweetie',
+        message: 'Good night and sweet dreams!'
+    })
+})
